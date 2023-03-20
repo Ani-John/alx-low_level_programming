e066c3767fa2e70432c2277a3dd99780b6fc9b50
ANI-JOHN <johnaniebiet@gmail.com>
2023-03-20 09:48:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,13 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		if (a == '9')
+		putchar(a);
+		if (a != '9')
 		{
-			putchar(a);
-		}
-		else
-		{
-			putchar(a);
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
