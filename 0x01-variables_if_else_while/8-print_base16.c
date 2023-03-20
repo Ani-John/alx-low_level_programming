ca3afd029a1cea63664c0680ccc36d5a83fd7d04
ANI-JOHN <johnaniebiet@gmail.com>
2023-03-20 07:53:57 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	char abc[] = "0123456789abcdef";
+
+		for (a = 0; a < 16; a++)
+		{	putchar(abc[a]);
+		}
+	return (0);
+}
