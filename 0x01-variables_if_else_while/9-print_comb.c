d82a845583a115c8dd222b7097577bffad30f739
ANI-JOHN <johnaniebiet@gmail.com>
2023-03-20 09:34:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible
+ * combinations of single-digit numbers.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a == '9')
+		{
+			putchar(a);
+		}
+		else
+		{
+			putchar(a);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
