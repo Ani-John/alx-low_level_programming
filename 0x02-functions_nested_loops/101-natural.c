801c98215fd4d4be524a771ba2afda9fa2d735bc
ANI-JOHN <johnaniebiet@gmail.com>
2023-03-22 19:30:21 -0700

    The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded)

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a program that computes and prints the sum of
+ * all the multiples of 3 or 5 below 1024 (excluded),
+ * followed by a new line.
+ * Return: the sum of all the multiples of 3 and 5
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 1; i < 20; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+	}
+	printf("the sum of all the multiples of 3 or");
+	printf("5 below 1024 (excluded) is: %d", j);
+	printf("\n");
+	return (j);
+}
