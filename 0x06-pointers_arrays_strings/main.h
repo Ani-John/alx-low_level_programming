55d8d22caed423ab1d26a0871c1c62a04d26abe1
ANI-JOHN <johnaniebiet@gmail.com>
2023-04-01 04:31:57 -0700

    okay

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -22,6 +22,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *st);
+char *rot13(char *str);
+
 
 
 #endif /* MAIN_H */
