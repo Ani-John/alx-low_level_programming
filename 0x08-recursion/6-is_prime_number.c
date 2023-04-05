0e3917b790c94724e18a5d827c6197bd0ec35ebf
Ani-John <Johnaniebiet@gmail.com>
2023-04-05 21:25:15 +0000

    ok

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * isp - determines if a number is prime
+ * @n: the number to be determined
+ * @i: recursive value  which brings the recurssion from n to 1.
+ * Return: 1 if prime, 0 if not.
+ */
+
+int isp(int n, int i)
+{
+	if (n <= 2)
+		return (0);
+
+	if (i == 1)
+		return (1);
+
+	if (!(n % (i)))
+		return (0);
+
+	isp(n, (i - 1));
+}
+
+
+/**
+ * is_prime_number - determines if a number is prime
+ * @n: the number to be determined
+ * Return: 1 if prime, 0 if not.
+ */
+
+int is_prime_number(int n)
+{
+	int i = (n - 1);
+
+	return (isp(n, i));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqr(int n, int root);
+int is_prime_number(int n);
 
 
 #endif /* MAIN_H */
