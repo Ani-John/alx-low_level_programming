ee6ab9c183e8e19469ce79debf49c51e9afa6ce6
Ani-John <Johnaniebiet@gmail.com>
2023-04-05 16:11:43 +0000

    okay

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 1;
+
+	if (*s == '\0')
+		return (0);
+
+	if (s[0])
+	{
+		len += _strlen_recursion(s + 1);
+		return (len);
+	}
+}
