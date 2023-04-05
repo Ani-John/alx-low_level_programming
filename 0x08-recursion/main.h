2ca14268281b3a2aaa1d2d4719b940b105be97f2
Ani-John <Johnaniebiet@gmail.com>
2023-04-05 15:43:38 +0000

    okay

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 
 char _putchar(char i);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 
 #endif /* MAIN_H */
