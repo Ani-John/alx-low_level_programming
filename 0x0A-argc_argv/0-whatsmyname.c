6112550c1b35d7ec8ed18b9df6d7ea85f713f005
Ani-John <Johnaniebiet@gmail.com>
2023-04-09 10:38:52 +0000

    okay

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - prints argv string
+ * @argc: argument counts
+ * @argv: argument vector
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Handles tasks on 0x0A. C - argc, argv
