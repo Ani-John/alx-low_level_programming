c40cbfe621f6dcbd4ff32bcad3e7ee061f549432
Ani-John <Johnaniebiet@gmail.com>
2023-04-11 09:45:43 +0000

    task 5

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+char _putchar(char i);
+void printchange(int amount);
+
+
+#endif /* MAIN_H */
--- 0x0A-argc_argv/printchange.c
+++ 0x0A-argc_argv/printchange.c
@@ -6,9 +6,10 @@
  * make change for an amount of money.
  *  @amount: the amount to find change for
  */
-void printchange(int amount)
+int printchange(int amount)
 {
 int num = 0;
+int amount = atoi(argv[1]);
 
 	while (amount > 0)
 	{
@@ -40,27 +41,5 @@ int num = 0;
 	}
 
 	printf("%d\n", num);
-}
-
-#include "main.h"
-/**
- * main - finds change to a given number
- * @argc: argument count
- * @argv: argument vector
- * Return: 1 if argc != 2, else 0.
- */
-
-int main(int argc, char *argv[])
-{
-	int amount;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	amount = atoi(argv[1]);
-	printchange(amount);
 	return (0);
 }
