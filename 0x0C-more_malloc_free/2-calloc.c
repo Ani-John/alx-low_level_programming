38a0327b9a0f974bcfdedb93f96d2e7ca1de6572
Ani-John <Johnaniebiet@gmail.com>
2023-04-13 20:05:21 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,10 +10,12 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
+
 	if ((nmemb == 0) || (size == 0))
 		return (0);
 
-	void *p = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
