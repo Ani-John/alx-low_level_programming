2dd8786a94ab9d0deb9087a2a1838b693cf9e11c
Ani-John <Johnaniebiet@gmail.com>
2023-05-08 21:23:26 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t a, b;
 
-	if (!(text_content != NULL))
+	if ((text_content == NULL))
 		return (-1);
 
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
