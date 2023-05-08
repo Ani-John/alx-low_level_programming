c68b143d6d71a360617095a7398d46ce05a985c1
Ani-John <Johnaniebiet@gmail.com>
2023-05-08 21:37:13 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	a = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	
+	if (a == -1)
+		return (-1);
 	b = write(a, text_content, strlen(text_content));
 	if (b == -1)
 		return (-1);
- if (a == -1)
-	                 return (-1);
 	close(a);
 
 	return (1);
