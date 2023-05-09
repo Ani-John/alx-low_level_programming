141e46582da6352b148560f6c3d43899b6b819a8
Ani-John <Johnaniebiet@gmail.com>
2023-05-09 08:40:22 +0000

    corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (b == -1)
-		return (-1);
-	if
-	b = write(a, text_content, strlen(text_content));
 	if (a == -1)
+		return (-1);
+	b = write(a, text_content, strlen(text_content));
+	if (b == -1)
 		return (-1);
 	close(a);
 
