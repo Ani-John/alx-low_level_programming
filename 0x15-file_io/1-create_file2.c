43ac26c0028289d69ba13ab142e67720367e7e04
Ani-John <Johnaniebiet@gmail.com>
2023-05-09 08:16:19 +0000

    test

--- 0x15-file_io/1-create_file2.c
+++ 0x15-file_io/1-create_file2.c
@@ -14,18 +14,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t a, b;
+	int o, w, len = 0;
 
-	if (!(text_content != NULL))
+	if (filename == NULL)
 		return (-1);
 
-	a = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (a == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	b = write(a, text_content, strlen(text_content));
-	if (b == -1)
-		return (-1);
-	close(a);
+
+	close(o);
 
 	return (1);
 }
