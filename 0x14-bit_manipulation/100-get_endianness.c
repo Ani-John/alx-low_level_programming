01e10608a76531d62b3047ad7f7527996fdfea62
Ani-John <Johnaniebiet@gmail.com>
2023-07-07 10:12:36 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianness - a function that checks the endianness.
+ *
+ * Return: Returns 1 if little endian,  OR returns 0 if b
+ * endian.
+ */
+int get_endianness(void)
+{
+	int k = 3;
+	int	num = k >> 1;
+	int *ptr;
+
+	ptr = &num;
+
+	if (*ptr == 1)
+		return (1);
+
+	return (0);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits -   a function that returns the number of bits
+ * you would need to flip to get from one number to another.
+ * given index.
+ * @n: a pointer to the first number whose whose binary is to
+ * be searched.
+ * @m: a pointer to the second number whose whose binary is to
+ * be searched.
+ *
+ * Return: the number of bits you would need to flip to get
+ * from one number to another.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int sum = 0, num;
+
+	num = n ^ m;
+
+	while (num > 0)
+	{
+		sum = sum + (num & 1);
+		num = num >> 1;
+	}
+	return (sum);
+}
